c6d9f8a233364a4e3d58097b8d158bb233ef836b
Rahul Kumar <rahulkumar151995@gmail.com>
2020-11-07 02:37:47 +0530

    pagination

--- src/controllers/CommentRouter.ts
+++ src/controllers/CommentRouter.ts
@@ -8,17 +8,14 @@ export class CommentController {
 
         const content = req.body.content;
         const post = req.post;
-        console.log("post", post);
-        let postObj: any = { _id: post._id, comments: [] }
-
         try {
             const comment = new Comment({
                 content: content,
                 created_at: new Date(),
                 updated_at: new Date()
             });
-            postObj.comments.push(comment);
-            await Promise.all([comment.save(), postObj.save()]);
+            post.comments.push(comment);
+            await Promise.all([comment.save(), post.save()]);
             res.send(comment);
 
         } catch (e) {
--- src/controllers/PostControllers.ts
+++ src/controllers/PostControllers.ts
@@ -25,10 +25,42 @@ export class PostController {
 
     static async getPostByUser(req, res, next) {
         const userId = req.user.user_id;
+
+
+        //PAGINATION LOGIC BEGIN
+
+        const page = parseInt(req.query.page) || 1;
+        const perPage = parseInt(req.query.perPage) || 2;
+        let currentPage = page;
+        let prevPage = page === 1 ? null : page - 1;
+        let pageToken = page + 1;  // next page
+        let totalPage;
+
+        //PAGINATION LOGIC END
+
+
         try {
             // const post = await Post.find({ user_id: userId }).exec(); //When not populate the comment.
-            const post = await Post.find({ user_id: userId }).populate('comments').exec(); // When need to populate the comment also.
-            res.send(post);
+            // const post = await Post.find({ user_id: userId }).populate('comments').exec(); // When need to populate the comment also.
+            // res.send(post);
+
+            // estimatedDocumentCount is efficent than countDocument but did not accept criteria fields 
+            const postCount = await Post.countDocuments({ user_id: userId });
+            totalPage = Math.ceil(postCount / perPage);
+
+            if (totalPage === page || totalPage === 0) {
+                pageToken = null;
+            }
+            const posts = await Post.find({ user_id: userId }, { __v: 0, user_id: 0 })
+                .populate('').limit(perPage).skip((perPage * page) - perPage);
+
+            res.json({
+                post: posts,
+                totalPage: totalPage,
+                currentPage: currentPage,
+                prevPage: prevPage,
+                nextPage: pageToken
+            })
 
         } catch (e) {
             next(e);
